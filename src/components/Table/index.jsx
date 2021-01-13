695ecb9e94e4ac53a0ead8d87ac5f0e6f7382468
jeanmendescs <mendes.jean.cs@gmail.com>
2021-01-13 18:08:59 -0300

    renderBody func fix

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -3,51 +3,17 @@ import React from "react";
 const Table = ({ data, header, pageTitle }) => {
   const renderBody = () => {
     return data.map(({ userId, id, title, body, completed }) => {
-      if (completed) {
-        return (
-          <tr key={id}>
-            <td>{userId}</td>
-            <td>{id}</td>
-            <td>{title}</td>
-            <td>{`${completed}`}</td>
-          </tr>
-        );
-      } else {
-        return (
-          <tr key={id}>
-            <td>{userId}</td>
-            <td>{id}</td>
-            <td>{title}</td>
-            <td>{body}</td>
-          </tr>
-        );
-      }
+      return (
+        <tr key={id}>
+          <td>{userId}</td>
+          <td>{id}</td>
+          <td>{title}</td>
+          {body && <td>{`${body}`}</td>}
+          {completed && <td>{`${completed}`}</td>}
+        </tr>
+      );
     });
   };
-
-  // const renderBody = () => {
-  //   return data.map((value, index) => {
-  //     console.log(value);
-  //     return (
-  //       <tr key={index}>
-  //         <td>{value}</td>
-  //       </tr>
-  //     );
-  //   });
-  // };
-
-  // const renderHeader = () => {
-  //   return header.map((columnTitle) => {
-  //     return (
-  //       <tr key={id}>
-  //         <td>{userId}</td>
-  //         <td>{id}</td>
-  //         <td>{title}</td>
-  //         <td>{body}</td>
-  //       </tr>
-  //     );
-  //   });
-  // };
 
   const renderHeader = () => {
     return header.map((columnTitle, index) => (
