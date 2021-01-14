777b11c43abafab8679e8425b60e731f83a12e3c
jeanmendescs <mendes.jean.cs@gmail.com>
2021-01-14 18:32:38 -0300

    home styling

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
-import ToDoImg from "../../img/Todos.jpg";
-import AlbumsImg from "../../img/Albums.jpg";
-import PostsImg from "../../img/Posts.jpg";
+import Classnames from "classnames";
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <div className={styles.cardImg}>
-          <img src={PostsImg} alt="asd"></img>
-        </div>
-        <Link to="/posts">Posts</Link>
+        <Link
+          className={Classnames(styles.cardImg, styles.postsImg)}
+          to="/posts"
+        >
+          Posts
+        </Link>
       </div>
       <div className={styles.card}>
-        <div className={styles.cardImg}>
-          <img src={AlbumsImg} alt="asd"></img>
-        </div>
-        <Link to="/albums">Albums</Link>
+        <Link
+          className={Classnames(styles.cardImg, styles.albumsImg)}
+          to="/albums"
+        >
+          Albums
+        </Link>
       </div>
       <div className={styles.card}>
-        <div className={styles.cardImg}>
-          <img src={ToDoImg} alt="asd"></img>
-        </div>
-        <div>
-          <Link to="/todos">To-Dos</Link>
-        </div>
+        <Link
+          className={Classnames(styles.cardImg, styles.todosImg)}
+          to="/todos"
+        >
+          To-Dos
+        </Link>
       </div>
     </div>
   );
